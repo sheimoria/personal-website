70b295b902f8812b676f6d31c430cf8ee8cbb25f
Shem Maleriado <shem.maleriado@gmail.com>
2021-10-01 02:58:02 +0800

    refactor: contact info styles

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -17,9 +17,8 @@ const Intro = () => {
         </p>
         <p>Have a great day!</p>
       </div>
-      <div className="flex flex-col gap-6">
-        <h1 className="px-0">Connect with me!</h1>
-
+      <div className="flex flex-col gap-6 p-8 border border-gray-300 rounded-lg">
+        <h1 className="px-0">Contact me</h1>
         <div className="flex items-center gap-8">
           <svg
             onClick={() => router.push('mailto: shem.maleriado@gmail.com')}
@@ -51,7 +50,7 @@ const Intro = () => {
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
           </svg>
         </div>
-        <h1 className="px-0">Happy tinkering with:</h1>
+        <h1 className="px-0">Favourite technologies</h1>
         <div className="flex items-center gap-8">
           {tech.map((tech, index) => (
             <div
