b9c38a281b21f9adf7ea55dfabdccbd474cd19ff
Shem Maleriado <shem.maleriado@gmail.com>
2022-04-05 02:09:39 +0800

    refactor: update resume link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -44,7 +44,7 @@ const Header = () => {
             Art&Design
           </a>
           <a
-            href="https://drive.google.com/file/d/1qwqvHZsvSc15-skze3NwWs26OiqZc4tM/view?usp=sharing"
+            href="https://docs.google.com/document/d/1KkM6z7mh1MqOmxxxwrmVgwo7zXU7S9Xvez6RMNLnaJY/edit?usp=sharing"
             target="_blank"
             rel="noreferrer"
             className="px-6 py-4 text-gray-800 transition-colors bg-gray-200 rounded-lg hover:text-gray-900 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-gray-100 dark:hover:text-white"
@@ -69,16 +69,20 @@ const Header = () => {
         </Fade>
         {/* Mobile */}
       </nav>
-
       <div className="flex items-center flex-none gap-6 lg:hidden">
         <Fade cascade direction="down" duration={600}>
           <MailIcon
-            onClick={() => router.push('mailto: shem.maleriado@gmail.com')}
+            onClick={() =>
+              window.open('mailto: shem.maleriado@gmail.com', '_blank')
+            }
             className="w-6 h-6 text-gray-800 transition-colors cursor-pointer hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100"
           />
           <svg
             onClick={() =>
-              router.push('https://www.linkedin.com/in/shemmaleriado/')
+              window.open(
+                'https://www.linkedin.com/in/shemmaleriado/',
+                '_blank'
+              )
             }
             fill="currentColor"
             viewBox="-2.5 -2.5 20 20"
@@ -87,7 +91,9 @@ const Header = () => {
             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
           </svg>
           <svg
-            onClick={() => router.push('https://github.com/sheimoria')}
+            onClick={() =>
+              window.open('https://github.com/sheimoria', '_blank')
+            }
             fill="currentColor"
             viewBox="-2.5 -2.5 20 20"
             className="w-6 h-6 text-gray-800 transition-colors cursor-pointer hover:text-gray-900 dark:text-gray-200 dark:hover:text-gray-100"
